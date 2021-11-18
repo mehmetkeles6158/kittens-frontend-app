2e91a8994bbb24efc45a5c31663adde79759ec17
Mehmet <mehmetk6158@gmail.com>
2021-11-17 20:30:42 -0500

    add create action to frontend

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import KittensNew from '../views/KittensNew.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/kittens/new',
+    name: 'kittens-create',
+    component: KittensNew
   },
   {
     path: '/about',
---
+++ src/views/KittensNew.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="Create - Kitten">
+    <form v-on:submit.prevent="submit()">
+      <h1>Signup</h1>
+      <ul>
+        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+      </ul>
+      <div>
+        <label>Name:</label>
+        <input type="text" v-model="newKittenParams.name" />
+      </div>
+      <div>
+        <label>Breed:</label>
+        <input type="breed" v-model="newKittenParams.breed" />
+      </div>
+      <div>
+        <label>Age:</label>
+        <input type="Age" v-model="newKittenParams.age" />
+      </div>
+      <input type="submit" value="Submit" />
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      newKittenParams: {},
+      errors: [],
+    };
+  },
+  methods: {
+    submit: function () {
+      axios
+        .post("/kittens", this.newKittenParams)
+        .then((response) => {
+          console.log(response.data);
+          this.$router.push("/kittens");
+        })
+        .catch((error) => {
+          this.errors = error.response.data.errors;
+        });
+    },
+  },
+};
+</script>
